7f429dcbd47cd223c3dd86c048b1295dda2b5457
Amgde22 <magidembarek@gmail.com>
2025-07-31 18:55:04 +0100

    edits

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,11 +3,11 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
             <path fill="currentColor" d="M20 6H4V4h16zm-4.31 8H14v1.69c-.63.95-1 2.08-1 3.31c0 .34.04.67.09 1H4v-6H3v-2l1-5h16l1 5v1.35c-.63-.22-1.3-.35-2-.35c-1.23 0-2.36.37-3.31 1M12 14H6v4h6zm9.34 1.84l-3.59 3.59l-1.59-1.59L15 19l2.75 3l4.75-4.75z" />
         </svg>
-        our products :
+        Notre sélection :
     </h1>
 
-    <SelectNav :items="tags" select-all-label="All"
-    @select="e => {selectedTag = e}" />
+    <!-- <SelectNav :items="tags" select-all-label="All"
+    @select="e => {selectedTag = e}" /> -->
     
     <section :class="[
             'product-section',
@@ -30,8 +30,8 @@
 </section>
 
 <div v-if="isSection" class="center">
-    <sl-button class="button" variant="neutral" size="large" href="/Produits">
-        ...Discover more
+    <sl-button class="button" variant="neutral" size="large" href="/produits">
+        Découvrez plus  
         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 48 48">
         <defs>
             <mask id="ipSRightC0">
